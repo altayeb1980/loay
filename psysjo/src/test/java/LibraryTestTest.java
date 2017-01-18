7b8e7bbb610ae7f6e29b890e2d6f71a1e46c450c
Altayeb Saadeh <loay.saada@gmail.com>
2017-01-18 23:29:40 +0200

    add a new test

---
+++ psysjo/src/test/java/LibraryTestTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by u259 on 1/18/2017.
+ */
+public class LibraryTestTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testSomeLibraryMethod() throws Exception {
+
+    }
+
+}
